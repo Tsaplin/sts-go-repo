25caad0f1daaa065e96b5897e2d4004c697dd575
stsaplin <stsaplin@diasoft.ru>
2024-09-07 22:33:27 +0300

    Добился запуска функции main

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,6 +1,6 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/Tsaplin/sts-go-repo/hw02_unpack_string
 
-go 1.22
+go 1.22.1
 
 require github.com/stretchr/testify v1.7.0
 
--- hw02_unpack_string/main.go
+++ hw02_unpack_string/main.go
@@ -1,12 +1,19 @@
-package hw02unpackstring
+package main
 
 import (
 	"errors"
+	"fmt"
 )
+
+func main() {
+	fmt.Println("hw02unpackstring - function main")
+	Unpack("s1")
+}
 
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(_ string) (string, error) {
 	// Place your code here.
+	fmt.Println("hw02unpackstring - function Unpack")
 	return "", nil
 }
