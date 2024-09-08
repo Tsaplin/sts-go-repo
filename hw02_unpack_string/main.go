d394c6f4aa06c89504f9a6d8dc41d2fd18db1f88
stsaplin <stsaplin@diasoft.ru>
2024-09-08 15:37:34 +0300

    Замечания линтеров

--- hw02_unpack_string/main.go
+++ hw02_unpack_string/main.go
@@ -10,11 +10,14 @@ import (
 
 func main() {
 	fmt.Println("hw02unpackstring - function main")
-	Unpack("")
+	str, err := Unpack("a4bc2d5e")
+	fmt.Println("str=", str)
+	fmt.Println("err=", err)
 }
 
 var ErrInvalidString = errors.New("invalid string")
 
+// Функция срезает последний исмвол строки и возвращает оставшуюся строку
 func RemoveChar(word string) (result string) {
 	length := utf8.RuneCountInString(word)
 	if length == 0 || length == 1 {
@@ -31,29 +34,18 @@ func Unpack(str string) (string, error) {
 	// Place your code here.
 	fmt.Println("hw02unpackstring - function Unpack")
 
-	//tmpstr := RemoveChar("a2")
-
-	//tmpstr := strings.Repeat("a", 0)
-	//fmt.Println("tmpstr=", tmpstr)
-
 	arr := strings.Split(str, "")
 	length := len(arr)
-
-	//fmt.Println("length=", length)
 
 	if length == 0 {
 		return "", nil
 	}
 
-	//Если 0-й элемент явл-ся цифрой, то выходим с ошибкой
+	// Если 0-й элемент явл-ся цифрой, то выходим с ошибкой
 	_, err := strconv.Atoi(arr[0])
 
-	//fmt.Println("Symbol=", number)
-	//fmt.Println("Error=", err)
-
 	if err == nil {
-		//fmt.Println("Exit by Error")
-		return "", err
+		return "", ErrInvalidString
 	}
 
 	var resultStr string = arr[0]
@@ -61,12 +53,9 @@ func Unpack(str string) (string, error) {
 	for i := 1; i < length; i++ {
 		number, err := strconv.Atoi(arr[i])
 
-		//fmt.Println("Cycle Symbol=", number)
-		//fmt.Println("Cycle Error=", err)
-
 		// Тут значение isNumber еще от прошлой итерации. Если прошлый символ число и текущий число, то выходим с ошибкой
-		if err == nil && isNumber == true {
-			return "", err
+		if err == nil && isNumber {
+			return "", ErrInvalidString
 		}
 
 		if err == nil {
@@ -77,21 +66,20 @@ func Unpack(str string) (string, error) {
 
 		// Если символ не цифра, то укажем его в результирующей строке
 		if err != nil {
-			resultStr = resultStr + arr[i]
+			resultStr += arr[i]
 		} else { // Если же цифра, то повторим прошлый элемент кол-во раз = цифре
 			// Если цифра = 0, то надо срезать последний символ
 			if number == 0 {
 				resultStr = RemoveChar(resultStr)
 			} else {
 				// Если не 0, то повторить number-1 раз прошлый символ
-				resultStr = resultStr + strings.Repeat(arr[i-1], number-1)
+				resultStr += strings.Repeat(arr[i-1], number-1)
 			}
 		}
 
 	}
 
-	// fmt.Println(arr)
-	fmt.Println("resultStr=", resultStr)
+	// fmt.Println("resultStr=", resultStr)
 
 	return resultStr, nil
 }
