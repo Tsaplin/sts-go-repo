2bc9a49e0412d317ed4d168336f158f29fd80bbf
stsaplin <stsaplin@diasoft.ru>
2024-09-08 17:03:57 +0300

    Добавление тестов

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,10 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "f3b", expected: "fffb"},
+		{input: "-2c", expected: "--c"},
+		{input: "g!2c", expected: "g!!c"},
+		{input: "qwe3", expected: "qweee"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
