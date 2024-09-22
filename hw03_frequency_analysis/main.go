db3a86451fb455c8f66c319049d39eaae9094b7a
stsaplin <stsaplin@diasoft.ru>
2024-09-22 13:03:36 +0300

    Компиляция без ошибок

---
+++ hw03_frequency_analysis/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hw03_frequency_analysis - function main")
+}
+
+func Top10(_ string) []string {
+	// Place your code here.
+	return nil
+}
