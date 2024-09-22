940eaa87c808b03ddd164961a030ed984c59da5a
stsaplin <stsaplin@diasoft.ru>
2024-09-22 17:39:20 +0300

    Работает но тест не проходит

--- hw03_frequency_analysis/main.go
+++ hw03_frequency_analysis/main.go
@@ -8,16 +8,63 @@ import (
 
 func main() {
 	fmt.Println("hw03_frequency_analysis - function main")
-	//inputStr := "cat and dog, one dog,two cats and one man and dog"
-	inputStr := ""
 
-	Top10(inputStr)
+	// inputSrc := make([]WordFreq, 6)
+	// inputSrc[0] = WordFreq{"boy", 2}
+	// inputSrc[1] = WordFreq{"dog", 2}
+	// inputSrc[2] = WordFreq{"and", 2}
+	// inputSrc[3] = WordFreq{"girl", 3}
+	// inputSrc[4] = WordFreq{"girll", 3}
+	// inputSrc[5] = WordFreq{"abc", 1}
+	//lexycoGraphicSort(inputSrc)
+
+	//inputStr := "boy dog cat and"
+	inputStr := "cat and dog, one dog,two cats and one man and dog"
+	//inputStr := ""
+
+	/*
+		var inputStr = `Как видите, он  спускается  по  лестнице  вслед  за  своим
+		другом   Кристофером   Робином,   головой   вниз,  пересчитывая
+		ступеньки собственным затылком:  бум-бум-бум.  Другого  способа
+		сходить  с  лестницы  он  пока  не  знает.  Иногда ему, правда,
+			кажется, что можно бы найти какой-то другой способ, если бы  он
+		только   мог   на  минутку  перестать  бумкать  и  как  следует
+		сосредоточиться. Но увы - сосредоточиться-то ему и некогда.
+			Как бы то ни было, вот он уже спустился  и  готов  с  вами
+		познакомиться.
+		- Винни-Пух. Очень приятно!
+			Вас,  вероятно,  удивляет, почему его так странно зовут, а
+		если вы знаете английский, то вы удивитесь еще больше.
+			Это необыкновенное имя подарил ему Кристофер  Робин.  Надо
+		вам  сказать,  что  когда-то Кристофер Робин был знаком с одним
+		лебедем на пруду, которого он звал Пухом. Для лебедя  это  было
+		очень   подходящее  имя,  потому  что  если  ты  зовешь  лебедя
+		громко: "Пу-ух! Пу-ух!"- а он  не  откликается,  то  ты  всегда
+		можешь  сделать вид, что ты просто понарошку стрелял; а если ты
+		звал его тихо, то все подумают, что ты  просто  подул  себе  на
+		нос.  Лебедь  потом  куда-то делся, а имя осталось, и Кристофер
+		Робин решил отдать его своему медвежонку, чтобы оно не  пропало
+		зря.
+			А  Винни - так звали самую лучшую, самую добрую медведицу
+		в  зоологическом  саду,  которую  очень-очень  любил  Кристофер
+		Робин.  А  она  очень-очень  любила  его. Ее ли назвали Винни в
+		честь Пуха, или Пуха назвали в ее честь - теперь уже никто  не
+		знает,  даже папа Кристофера Робина. Когда-то он знал, а теперь
+		забыл.
+			Словом, теперь мишку зовут Винни-Пух, и вы знаете почему.
+			Иногда Винни-Пух любит вечерком во что-нибудь поиграть,  а
+		иногда,  особенно  когда  папа  дома,  он больше любит тихонько
+		посидеть у огня и послушать какую-нибудь интересную сказку.
+			В этот вечер...`
+	*/
+
+	res := Top10(inputStr)
+	fmt.Println("Топ 10 слов: ", res)
 }
 
 type WordFreq struct {
-	number int    // Номер слова в исходном массиве
-	word   string // Слово
-	count  int    // Кол-во вхождений слова
+	word  string // Слово
+	count int    // Кол-во вхождений слова
 }
 
 /*
@@ -34,6 +81,66 @@ func isRepeatOfWordInArray(inputWord string, inputArr []WordFreq) bool {
 	}
 
 	return res
+}
+
+/*
+Функция проверяет, содержит ли переданный массив кол-во указания count
+*/
+func isRepeatOfCountInArray(count int, inputArr []WordFreq) bool {
+	res := false
+	arrLength := len(inputArr)
+
+	for i := 0; i < arrLength; i++ {
+		if inputArr[i].count == count {
+			return true
+		}
+	}
+
+	return res
+}
+
+/*
+Функция возвращает массив, отсортированный лексикографически внутри тех слайсов элементов,
+для которых частота указания в тексте совпадает.
+*/
+func lexycoGraphicSort(inputArr []WordFreq) []WordFreq {
+	outputArr := make([]WordFreq, 0)
+
+	for i := 0; i < len(inputArr); i++ {
+		currentCount := inputArr[i].count
+
+		if isRepeatOfCountInArray(currentCount, outputArr) {
+			continue
+		}
+
+		currentIndexStart := i // Самый ранний номер элемента с данным значением currentCount (частоты указания слова)
+		currentIndexEnd := i   // Самый поздний номер элемента с данным значением currentCount (частоты указания слова)
+		currentWords := make([]string, 0)
+
+		for j := i + 1; j < len(inputArr); j++ {
+			if currentCount == inputArr[j].count {
+				currentIndexEnd = j
+			}
+		}
+
+		// Лексикографическая сортировка нужна лишь в том случае, если есть несколько элементов (слов) с одинаковым значением частоты использования
+		if currentIndexEnd > currentIndexStart {
+			for k := currentIndexStart; k <= currentIndexEnd; k++ {
+				currentWords = append(currentWords, inputArr[k].word)
+			}
+			sort.Strings(currentWords)
+
+			// Заменим элемент массива, чтобы соблюдалась сортировка слов
+			for k := currentIndexStart; k <= currentIndexEnd; k++ {
+				//outputArr[k] = WordFreq{currentWords[k], currentCount}
+				outputArr = append(outputArr, WordFreq{currentWords[k-currentIndexStart], currentCount})
+			}
+		} else {
+			outputArr = append(outputArr, inputArr[i])
+		}
+	}
+
+	return outputArr
 }
 
 func Top10(inputStr string) []string {
@@ -63,7 +170,7 @@ func Top10(inputStr string) []string {
 			}
 		}
 
-		wordFreqArray = append(wordFreqArray, WordFreq{i, elementValue, count})
+		wordFreqArray = append(wordFreqArray, WordFreq{elementValue, count})
 	}
 
 	// Отсортируем данные по кол-ву вхождений каждого слова. Первые элементы - это самые популярные слова
@@ -78,13 +185,13 @@ func Top10(inputStr string) []string {
 	}
 	topWordFreqArray := wordFreqArray[0:resultLength]
 
+	// Лексикографическая сортировка
+	lexicalSortedTopWordFreqArray := lexycoGraphicSort(topWordFreqArray)
+
 	var res []string
 	for k := 0; k < resultLength; k++ {
-		res = append(res, topWordFreqArray[k].word)
+		res = append(res, lexicalSortedTopWordFreqArray[k].word)
 	}
-
-	// fmt.Println("Top10 - finish")
-	// fmt.Println(topWordFreqArray)
 
 	return res
 }
