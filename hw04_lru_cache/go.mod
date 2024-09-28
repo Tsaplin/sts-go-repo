596960c081106087935a83cd84bf90776816911d
stsaplin <stsaplin@diasoft.ru>
2024-09-28 14:49:34 +0300

    Добился компиляции

--- hw04_lru_cache/go.mod
+++ hw04_lru_cache/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw04_lru_cache
+module github.com/Tsaplin/sts-go-repo/hw04_lru_cache
 
 go 1.22
 
---
+++ hw04_lru_cache/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hw04_lru_cache - function main")
+}
+
+type Key string
+
+type Cache interface {
+	Set(key Key, value interface{}) bool
+	Get(key Key) (interface{}, bool)
+	Clear()
+}
+
+type lruCache struct {
+	Cache // Remove me after realization.
+
+	capacity int
+	queue    List
+	items    map[Key]*ListItem
+}
+
+func NewCache(capacity int) Cache {
+	return &lruCache{
+		capacity: capacity,
+		queue:    NewList(),
+		items:    make(map[Key]*ListItem, capacity),
+	}
+}
