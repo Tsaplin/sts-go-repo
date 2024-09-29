394074a35965802a01d0a39c5e05e210b3998026
stsaplin <stsaplin@diasoft.ru>
2024-09-29 15:26:08 +0300

    Реализовал методы работы с кэшем

--- hw04_lru_cache/main.go
+++ hw04_lru_cache/main.go
@@ -7,11 +7,6 @@ import (
 func main() {
 	fmt.Println("hw04_lru_cache - function main")
 
-	//newSlice := make([]int, 1)
-	//newSlice[0] = ListItem{nil, nil, nil}
-	//fmt.Println("newSlice = ", newSlice)
-
-	//var t = myList{nil, newSlice}
 	t := NewList()
 	//var res ListItem = *t.PushFront(10) // [10]
 	//t.PushFront(10) // [10]
@@ -46,7 +41,7 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
+	//Cache // Remove me after realization.
 
 	capacity int
 	queue    List
@@ -59,4 +54,45 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+// Получить значение из кэша по ключу
+func (t *lruCache) Get(key Key) (interface{}, bool) {
+	var item *ListItem = t.items[key]
+	if item == nil {
+		return nil, false
+	}
+	t.queue.PushFront(item.Value)
+
+	return item.Value, true
+}
+
+// Добавить значение в кэш по ключу
+func (t *lruCache) Set(key Key, value interface{}) bool {
+	var item *ListItem = t.items[key]
+
+	// Добавляемый элемент отсутствует в словаре
+	if item == nil {
+		if t.queue.Len() > t.capacity {
+			tail := t.queue.Back()
+			t.queue.Remove(tail)
+			//t.items[] = nil
+			delete(t.items, key) // ??? Какой ключ у последнего элемента ?
+		}
+		t.queue.PushFront(value)
+		addedItem := t.queue.Front()
+		t.items[key] = addedItem
+		return false
+	}
+
+	// Добавляемый элемент присутствует в словаре
+	item.Value = value
+	t.queue.MoveToFront(item)
+	return true
+}
+
+// Очистить кэш
+func (t *lruCache) Clear() {
+	// ??? Нужно ли чистить связный список
+	clear(t.items)
 }
