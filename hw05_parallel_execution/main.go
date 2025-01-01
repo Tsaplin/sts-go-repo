1c46f06afcaf44d0560f156c93756f0587dd71d1
stsaplin <stsaplin@diasoft.ru>
2025-01-01 23:15:39 +0300

    Добавлена остановка горутин путем обработки потока

--- hw05_parallel_execution/main.go
+++ hw05_parallel_execution/main.go
@@ -32,7 +32,9 @@ func main() {
 	tasksOfJob = append(tasksOfJob, nil)
 	tasksOfJob = append(tasksOfJob, nil)
 
-	Run(tasksOfJob, 5, 1)
+	Run(tasksOfJob, 6, 1)
+	//time.Sleep(2 * time.Second)
+	fmt.Println("Finish Count of active go routines in main = ", runtime.NumGoroutine())
 	fmt.Println("hw05_parallel_execution - main finish")
 }
 
@@ -50,7 +52,7 @@ func generator(tasks []Task) <-chan Task {
 	return c
 }
 
-// Созание мапы с номерами "ошибочных" значений индекса (ключа)
+// Создание мапы с номерами "ошибочных" значений индекса (ключа)
 func fillErrIndexMap() map[int]int {
 	errIndexMap := make(map[int]int)
 	errIndexMap[3] = 1
@@ -67,47 +69,64 @@ func Run(tasks []Task, n, m int) error {
 	ch := generator(tasks)
 	fmt.Println("Канал=", ch)
 
+	// Создадим канал для остановки данной функции из рутин
+	stopCh := make(chan bool)
+
 	errIndexMap := fillErrIndexMap()
 	errCount := 0
-	tooManyErrors := false
 
 	wg := sync.WaitGroup{}
 	wg.Add(n)
 	mu := sync.Mutex{}
 	for i := 0; i < n; i++ {
 		// Функция обработки задач из канала
-		go func(c <-chan Task) {
-			defer wg.Done()
-			fmt.Println("i = " + strconv.Itoa(i) + " Exec of go routine")
-			// Если индекс относится к одному из списка "ошибочных", то выходим из функции
-			_, isErrorIndex := errIndexMap[i]
-			if isErrorIndex {
-				mu.Lock()
-				errCount++
-				mu.Unlock()
+		go func(c <-chan Task, stopChRead <-chan bool, stopChWrite chan<- bool) error {
+			//fmt.Println("Iteration i = " + strconv.Itoa(i) + " n=" + strconv.Itoa(n))
+			wg.Done()
+			for {
+				select {
+				default:
+					// Выполнение работы в горутине
+					//defer wg.Done()
+					fmt.Println("i = " + strconv.Itoa(i) + " Exec of go routine")
+					// Если индекс относится к одному из списка "ошибочных", то выходим из функции
+					_, isErrorIndex := errIndexMap[i]
+					if isErrorIndex {
+						mu.Lock()
+						errCount++
+						mu.Unlock()
 
-				fmt.Println("errCount = " + strconv.Itoa(errCount))
-				if errCount >= m {
-					fmt.Println("i = " + strconv.Itoa(i) + " tooManyErrors=true")
-					tooManyErrors = true
+						fmt.Println("errCount = " + strconv.Itoa(errCount))
+
+						return nil
+					}
+
+					if errCount >= m {
+						fmt.Println("Отправлен сигнал об остановке")
+						stopChWrite <- true
+						return ErrErrorsLimitExceeded
+					}
+
+					// Обработка задач канала
+					for t := range c {
+						fmt.Println("i = "+strconv.Itoa(i)+" Treatment of Task. Task=", t)
+						//fmt.Println("i = "+strconv.Itoa(i)+" Count of active go routines = ", runtime.NumGoroutine())
+					}
+				case <-stopChRead:
+					// Получен сигнал об остановке
+					//defer wg.Done()
+					//fmt.Println("i = " + strconv.Itoa(i) + " Exec of go routine")
+					//fmt.Println("Остановлен")
+					return ErrErrorsLimitExceeded
 				}
-				return
 			}
-
-			for t := range c {
-				fmt.Println("i = "+strconv.Itoa(i)+" Treatment of Task. Task=", t)
-				//fmt.Println("i = "+strconv.Itoa(i)+" Count of active go routines = ", runtime.NumGoroutine())
-			}
-		}(ch)
+		}(ch, stopCh, stopCh)
 
 	}
 	wg.Wait()
 
-	if tooManyErrors {
-		fmt.Println("Stopped working because received " + strconv.Itoa(errCount) + " errors from tasks")
-		return ErrErrorsLimitExceeded
-	}
+	//time.Sleep(2 * time.Second)
 
-	fmt.Println("Finish Count of active go routines = ", runtime.NumGoroutine())
+	fmt.Println("Finish Count of active go routines in function Run = ", runtime.NumGoroutine())
 	return nil
 }
